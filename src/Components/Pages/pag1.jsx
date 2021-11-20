2c3aa379f73d2955fab818ee16d24aed45f3ad49
carnita <davidquinteiro7@gmail.com>
2021-11-20 12:46:05 -0300

    cambios

--- src/Components/Pages/pag1.jsx
+++ src/Components/Pages/pag1.jsx
@@ -52,19 +52,8 @@ const Page1 = () => {
         },
         cuadros:{
             display: "inline-block",
-            float: "left",
-            width:"35%",
-            color:"white",
-            border:"1px solid white",
-            padding:"10px",
-            margin:"20px",
-            background:"rgba(0, 0, 0, 0.452)",
-            borderRadius:"10px"
-        },
-        cuadros2:{
-            display: "inline-block",
-            float: "right",
-            width:"35%",
+            
+            width:"70%",
             color:"white",
             border:"1px solid white",
             padding:"10px",
@@ -86,11 +75,6 @@ const Page1 = () => {
                     <button onClick={Events}>EVENTOS</button>
                 </div>
                 <div style={style.cuadros}>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aut officiis. Veniam dolor quidem ex voluptatem adipisci, odit enim, temporibus quam tempore voluptatibus facere ut rem aperiam quaerat porro alias?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque harum fugit illo omnis quam reprehenderit voluptate assumenda eaque amet atque dolorum repellendus ullam blanditiis animi quia, ex dolorem esse ab.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione placeat aliquam laboriosam similique iste, voluptatibus veritatis minima odio possimus nulla inventore eveniet soluta aspernatur suscipit maiores autem, molestiae sit animi.</p>
-                </div>
-                <div style={style.cuadros2}>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aut officiis. Veniam dolor quidem ex voluptatem adipisci, odit enim, temporibus quam tempore voluptatibus facere ut rem aperiam quaerat porro alias?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque harum fugit illo omnis quam reprehenderit voluptate assumenda eaque amet atque dolorum repellendus ullam blanditiis animi quia, ex dolorem esse ab.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione placeat aliquam laboriosam similique iste, voluptatibus veritatis minima odio possimus nulla inventore eveniet soluta aspernatur suscipit maiores autem, molestiae sit animi.</p>
