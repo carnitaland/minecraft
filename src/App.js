57dc632a09a3e25ad234302fb463327f9f341af6
carnita <davidquinteiro7@gmail.com>
2021-11-22 19:34:34 -0300

    skeleton-Prueba

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Rutas from './Components/Rutas';
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
-          <Rutas />
-    
+      <Rutas />
     </div>
   );
 }
--- src/Components/Pages/pag1.jsx
+++ src/Components/Pages/pag1.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Fragment } from 'react/cjs/react.development'
 import { useNavigate } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
 import '../../App.css';
 
 const Page1 = () => {
 
     let navigate = useNavigate();
+    let [loading, setLoading] = useState(true)
 
     const Bases = () => {
         navigate("/basesycondiciones")
@@ -62,41 +64,88 @@ const Page1 = () => {
         }
     }
 
-    return(
-        <Fragment>
+    useEffect(()=>{
+        setTimeout(()=>{
+            setLoading(true)
+        },5000)
+    })
+    const Loader = () => {
+        return(
+            <Fragment>
+                <div className="logoInicio">
+                    <div className="logo"></div>
+                        <div>
+                            <button onClick={Bases}>REGLAMENTOS</button>
+                            <button onClick={Mision}>MISIONES</button>
+                            <button onClick={Progress}>PROGRESO</button>
+                            <button onClick={Events}>EVENTOS</button>
+                            <button onClick={Premio}>PREMIOS CARNITA-GAMES</button>
+                        </div>
+                        <div>
+                            <button onClick={Registrate}>REGISTRATE</button>
+                        </div>
+                        <div style={style.cuadros}>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aut officiis. Veniam dolor quidem ex voluptatem adipisci, odit enim, temporibus quam tempore voluptatibus facere ut rem aperiam quaerat porro alias?</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque harum fugit illo omnis quam reprehenderit voluptate assumenda eaque amet atque dolorum repellendus ullam blanditiis animi quia, ex dolorem esse ab.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione placeat aliquam laboriosam similique iste, voluptatibus veritatis minima odio possimus nulla inventore eveniet soluta aspernatur suscipit maiores autem, molestiae sit animi.</p>
+                        </div>
+                        <div style={style.cuadros}>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aut officiis. Veniam dolor quidem ex voluptatem adipisci, odit enim, temporibus quam tempore voluptatibus facere ut rem aperiam quaerat porro alias?</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque harum fugit illo omnis quam reprehenderit voluptate assumenda eaque amet atque dolorum repellendus ullam blanditiis animi quia, ex dolorem esse ab.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione placeat aliquam laboriosam similique iste, voluptatibus veritatis minima odio possimus nulla inventore eveniet soluta aspernatur suscipit maiores autem, molestiae sit animi.</p>
+                        </div>
+                        <div style={style.cuadros}>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aut officiis. Veniam dolor quidem ex voluptatem adipisci, odit enim, temporibus quam tempore voluptatibus facere ut rem aperiam quaerat porro alias?</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque harum fugit illo omnis quam reprehenderit voluptate assumenda eaque amet atque dolorum repellendus ullam blanditiis animi quia, ex dolorem esse ab.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione placeat aliquam laboriosam similique iste, voluptatibus veritatis minima odio possimus nulla inventore eveniet soluta aspernatur suscipit maiores autem, molestiae sit animi.</p>
+                        </div>
+                </div>
+    
+    
+                   
+            </Fragment>
+        
+        )
+    }
+    if(loading){
+        return(Loader())
+    }else {
+
+        return(
+            <Fragment>
             <div className="logoInicio">
                 <div className="logo"></div>
                     <div>
-                        <button onClick={Bases}>REGLAMENTOS</button>
-                        <button onClick={Mision}>MISIONES</button>
-                        <button onClick={Progress}>PROGRESO</button>
-                        <button onClick={Events}>EVENTOS</button>
-                        <button onClick={Premio}>PREMIOS CARNITA-GAMES</button>
+                        <button onClick={Bases}><Skeleton /></button>
+                        <button onClick={Mision}><Skeleton /></button>
+                        <button onClick={Progress}><Skeleton /></button>
+                        <button onClick={Events}><Skeleton /></button>
+                        <button onClick={Premio}><Skeleton /></button>
                     </div>
                     <div>
-                        <button onClick={Registrate}>REGISTRATE</button>
+                        <button onClick={Registrate}><Skeleton /></button>
                     </div>
                     <div style={style.cuadros}>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aut officiis. Veniam dolor quidem ex voluptatem adipisci, odit enim, temporibus quam tempore voluptatibus facere ut rem aperiam quaerat porro alias?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque harum fugit illo omnis quam reprehenderit voluptate assumenda eaque amet atque dolorum repellendus ullam blanditiis animi quia, ex dolorem esse ab.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione placeat aliquam laboriosam similique iste, voluptatibus veritatis minima odio possimus nulla inventore eveniet soluta aspernatur suscipit maiores autem, molestiae sit animi.</p>
+                        <p><Skeleton /></p>
+                        <p><Skeleton /></p>
+                        <p><Skeleton /></p>
                     </div>
                     <div style={style.cuadros}>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aut officiis. Veniam dolor quidem ex voluptatem adipisci, odit enim, temporibus quam tempore voluptatibus facere ut rem aperiam quaerat porro alias?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque harum fugit illo omnis quam reprehenderit voluptate assumenda eaque amet atque dolorum repellendus ullam blanditiis animi quia, ex dolorem esse ab.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione placeat aliquam laboriosam similique iste, voluptatibus veritatis minima odio possimus nulla inventore eveniet soluta aspernatur suscipit maiores autem, molestiae sit animi.</p>
+                        <p><Skeleton /></p>
+                        <p><Skeleton /></p>
+                        <p><Skeleton /></p>
                     </div>
                     <div style={style.cuadros}>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aut officiis. Veniam dolor quidem ex voluptatem adipisci, odit enim, temporibus quam tempore voluptatibus facere ut rem aperiam quaerat porro alias?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque harum fugit illo omnis quam reprehenderit voluptate assumenda eaque amet atque dolorum repellendus ullam blanditiis animi quia, ex dolorem esse ab.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione placeat aliquam laboriosam similique iste, voluptatibus veritatis minima odio possimus nulla inventore eveniet soluta aspernatur suscipit maiores autem, molestiae sit animi.</p>
+                        <p><Skeleton /></p>
+                        <p><Skeleton /></p>
+                        <p><Skeleton /></p>
                     </div>
             </div>
-
-
-               
         </Fragment>
-    )
+        )
+            
+    }
+
 }
 
 export default Page1;
